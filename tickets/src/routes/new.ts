5734506438db5ed97cfb72c874dc7c083831caa9
Yaroslav Batishchev <yaroslav.batishchev1@gmail.com>
2025-02-12 23:03:10 +0300

    added nats in ticket microservice, added events for creating tickets, port-forwarding for tests

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 
-import { currentUser, requireAuth, validateRequest } from "@yabtickets/common";
+import { requireAuth, validateRequest } from "@yabtickets/common";
 
 import { Ticket } from "../models/tickets";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 router.post('/api/tickets', requireAuth, [
@@ -17,6 +19,12 @@ router.post('/api/tickets', requireAuth, [
     userId: req.currentUser!.id
   });
   await ticket.save();
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  });
   res.status(201).send(ticket);
 });
 
