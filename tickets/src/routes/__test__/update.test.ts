22d3e6705ac2539c904375e10944f78ff86cd38b
Yaroslav Batishchev <yaroslav.batishchev1@gmail.com>
2025-02-18 18:27:09 +0300

    test: added tests for events

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { natsWrapper } from "../../nats-wrapper";
 
 it('returns a 404 if the provided id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -100,4 +101,32 @@ it('updates the ticket provided valid inputs', async () => {
     .send()
   expect(ticketResponse.body.title).toEqual(newTitle);
   expect(ticketResponse.body.price).toEqual(newPrice);
+});
+
+
+it('publishes an event', async () => {
+  const title = 'sdsd';
+  const price = 20;
+
+  const cookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title:'valid', 
+      price: 20
+    });
+  const id = response.body.id;
+  const newTitle = 'new title';
+  const newPrice = 100;
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: newTitle, 
+      price: newPrice
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
